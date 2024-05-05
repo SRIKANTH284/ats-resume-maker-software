f2faf1b4c6c1a4d6f6018923e490381341c3ce6c
BADAVATH SRIKANTH <80474350+SRIKANTH284@users.noreply.github.com>
2024-05-06 01:15:07 +0530

    Add files via upload

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import React, {  } from "react";
+import Builder from "./builder";
+
+const ResumeBuilder = () => {
+  return (
+    <>
+    <Builder />
+    </>
+  );
+};
+
+export default ResumeBuilder;
+
